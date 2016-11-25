d34c08f03321b319b1eaa6d7283db60c8f955dbb
momobadilak <momobadilak@somewhere>
2016-11-25 00:00:56 -0800

    initial checkin of skeletal code and docs

---
+++ README.md
@@ -0,0 +1,97 @@
+# Why
+I built this arduino project to mess around with some cheap ESP8266EX based
+WiFi module I picked up from amazon (here: https://amzn.com/B00O34AGSU )
+
+I want to be able to use the module in conjunction with other arduino
+boards and projects witht the ESP module acting primarily as a WIFI
+"modem"
+
+# Dependencies
+"andys_menu" - a small text based menu system. https://github.com/momobadilak/andys_menu
+
+Arduino micro board : https://www.arduino.cc/en/Main/ArduinoBoardMicro
+
+If you don't have an arduino micro, I *believe* this code could be modified to be
+use with a Leonardo or Mega board as they also have more than one Uart.
+
+# Use
+Build main/main.ino and run it on an arduino micro. Use your Host/PC to interact
+with the text menu that appears via the serial monitor (or better - use minicom or
+putty)
+
+# Schematic / Board setup / Pics
+TODO
+
+# Video
+TODO
+
+# Contact
+Drop me a note on twitter - @andyidsinga - if you find amu of this useful or
+need some help.
+
+
+# The authoritative source for ESP info
+You probably won't need to go much further than this site, I suspect
+this site and documentation has improved a lot over the past few
+years as this part appears to be farly popular.
+
+https://espressif.com/en/products/hardware/esp8266ex/resources
+
+
+***
+
+# Product description from amazon page
+Esp8266 Serial Wifi Wireless Transceiver Module _Esp-01_ Arduino Compatible Switch
+Description:
+802.11 b/g/n
+Serial/UART baud rate: 115200 bps
+Integrated TCP/IP protocol stack
+Input power: 3.3V
+I/O voltage tolerance: 3.6V Max
+Regular operation current draw: ~70mA
+Peak operating current draw: ~300mA
+Power down leakage current: <10?A
++19.5dBm output in 802.11b mode
+Flash Memory Size: 1MB (8Mbit)
+WiFi security modes: WPA, WPA2
+
+Other ESP modules / kits:
+
+Esp-02 http://www.amazon.com/dp/B00OSWG52I
+Esp-03 http://www.amazon.com/dp/B00OSWGA8M
+Esp-04 http://www.amazon.com/dp/B00OSWBQPE
+Esp-05 http://www.amazon.com/dp/B00OSWCGVW
+ESP-07 http://www.amazon.com/dp/B00PMURGFA
+ESP-12E http://www.amazon.com/dp/B00XJG7CYY
+ESP-12F http://www.amazon.com/dp/B015IF5TVQ
+UNO R3 kit http://www.amazon.com/dp/B017SCDJ64
+Kit without uno r3 board http://www.amazon.com/dp/B019A0MUHG
+Popular Kit http://www.amazon.com/dp/B00Q2CRM24
+
+
+***
+
+
+# Various links I've collected while researching these modules
+
+## Here are the technical links from the amazon item page:
+Arduino library: https://github.com/sleemanj/ESP8266_Simple
+
+## A good picture of the board and pin labels
+http://fabacademy.org/archives/2015/doc/networking-esp8266.html
+
+## Tool for uploading new firmware to the device itself (not the arduino)
+https://github.com/espressif/esptool
+
+## A youtube tutorial: 
+https://www.youtube.com/watch?v=qU76yWHeQuw 
+
+## Some interesting tech details
+https://nurdspace.nl/ESP8266#Introduction 
+
+## Another repo of ESP info (referenced from nurdspace)
+http://www.electrodragon.com/w/index.php?title=Category:ESP8266&redirect=no
+
+
+## Lots of stuff on Hackaday.com
+http://hackaday.com/?s=esp8266
---
+++ main/main.ino
@@ -0,0 +1,135 @@
+/*
+  main.ino for esp8266fun project
+
+  Theory of operation:
+  I'm using a menu system and a bunch of docs I've collected from the
+  web to figure out how to work with a cheap ESP8266 board I purchased from
+  amazon (https://amzn.com/B00O34AGSU)
+  
+  Power and logic levels:
+  Arduino power by 5V.
+  ESP board powered by external 3.3v supply
+  SN74LVC245A bus transceiver to shift from Arduino 5v to 3.3v logic levels.
+  
+*/
+
+#include <arduino.h>
+
+/* andys_menu from: https://github.com/momobadilak/andys_menu */
+#include <andys_menu.h>
+
+/*
+  Pin mapping:
+  Arduino Pin 2           >> Bus xcvr OE
+  Arduino Uart TX (Pin 1) >> Bus xcvr A1   >> B1 >> ESP URXD
+  Arduino Uart RX (Pin 0) <<<<<<<<<<<<<<<<<<<<<<<<< ESP UTXD
+  Arduino Pin 3           >> Bux xcvr A2   >> B3 >> ESP CH_PD 
+
+*/
+const int BUS_nOE    = 2;
+const int ESP_TX     = 1;
+const int ESP_RX     = 0;
+const int ESP_CHPD   = 3;
+
+/* create an instance of the menu class */
+AndysMenu theMenu;
+
+/* error handler function proto */
+void menu_ErrorHandler(const uint8_t code, const char* msg);
+
+/* 
+   These prompt menus can reused throughout the code whenever a prompt is required
+*/
+AndysPromptItem okPrompt[] = {
+  {'o', "ok"}
+};
+
+AndysPromptItem yesNoPrompt[] = {
+  {'y', "Yes"},
+  {'n', "No"}
+};
+
+/*
+  Main, Top level menu definition and function protos
+*/
+void topmenu_EnableESP(void* context);
+void topmenu_DisableESP(void* context);
+void topmenu_SingleCmd(void* context);
+AndysMenuItem topMenu[] = {
+  {'e', "enable ESP via ESP_CHPD", topmenu_EnableESP },
+  {'d', "disable ESP via ESP_CHPD", topmenu_DisableESP },
+  {'s', "single command entry mode", topmenu_SingleCmd }
+};
+
+/* normal arduino setup function called on boot */
+void setup() {
+
+  /* this is the Serial back to the Host machine -- not the ESP */
+  Serial.begin(115200);
+
+  Serial.println("+setup()");
+
+  /* setup pins */
+  pinMode(BUS_nOE,  OUTPUT);
+  pinMode(ESP_TX,   OUTPUT);
+  pinMode(ESP_RX,   INPUT);
+  pinMode(ESP_CHPD, OUTPUT);
+  
+  /* enable bus xcvr : two sides no longer isolated after this!*/
+  digitalWrite(BUS_nOE, LOW);
+
+  /* disable ESP ..for now*/
+  digitalWrite(ESP_CHPD,LOW);
+
+  Serial.println("finished pin setup");
+
+  /* setup the UART to be used with the ESP */
+  Serial1.begin(115200);
+  
+  Serial.println("-setup()");
+}
+
+/* arduino loop function called over and over forever */
+void loop() {
+
+  /* run the top level menu.
+     Note: I'm not passing anything for the context arg, but
+     that can be your goodies, and will not be touched by the menu code. */
+  theMenu.run(
+	      "=== Top Menu ===",
+	      topMenu, 
+	      sizeof(topMenu)/sizeof(AndysMenuItem), 
+	      (void*)0,
+	      menu_ErrorHandler);
+}
+
+/*****************************************************************************/
+/* menu item implementation                                                  */
+/*****************************************************************************/
+
+void topmenu_EnableESP(void* context){
+  Serial.print("+");Serial.println(__FUNCTION__);
+  Serial.println("TODO");
+}
+
+void topmenu_DisableESP(void* context){
+  Serial.print("+");Serial.println(__FUNCTION__);
+  Serial.println("TODO");
+}
+
+void topmenu_SingleCmd(void* context){
+  Serial.print("+");Serial.println(__FUNCTION__);
+  Serial.println("TODO");
+}
+
+/*
+  generic menu error handler - error code and message dumper
+*/
+void menu_ErrorHandler(const uint8_t code, const char* msg)
+{
+  Serial.print("+menu_ErrorHandler,");
+  Serial.print("code,"); 
+  Serial.print(code,HEX);
+  Serial.print(",msg,"); 
+  Serial.println(msg);
+}
